179147b42e59f185ce42a8a5ed39cbef313e70b7
SumitNawathe <50125892+SumitNawathe@users.noreply.github.com>
2022-12-15 19:55:34 -0500

    .

--- perform_test.py
+++ perform_test.py
@@ -2,9 +2,10 @@ import io
 from parse_diagram import *
 from codegen import CodeGen
 
-header = io.open("udp_header.txt").read()
+header = io.open("ip_header.txt").read()
 fields = parse_diagram(header)
 cleaned_fields = collect_options(fields)
 final_fields = make_abbr(cleaned_fields)
+print(final_fields)
 codegen = CodeGen()
-print(codegen.gen_class_code('UDP', final_fields))
+print(codegen.gen_class_code('IP', final_fields))
